6fa86e480bde2bf58b8e70a509ba0e76444b5f42
wolf4ok <demon.roge@gmail.com>
2021-07-21 01:46:22 +0300

    test 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'post/index'
+  get 'post/:id', to: 'posts#show'
+
+  root to: "post#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
